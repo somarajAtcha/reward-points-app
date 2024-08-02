f32805b90b8626622bbd986f763443cc2e89f072
Susena Varma <susenavishnu@Susenas-MacBook-Air.local>
2024-08-02 16:55:08 +0530

    restore files

---
+++ src/components/customerTransactions/customerTransactions.js
@@ -0,0 +1,34 @@
+import { TRANSACTIONS_TABLE_HEADINGS } from "../../constants";
+
+/** Render the  transactions with reward points table */
+const CustomerTransactions = ({ rewardPointsData }) => {
+    return (<table className="rewards-table">
+        <thead>
+            <tr >
+                {
+                    Object.keys(TRANSACTIONS_TABLE_HEADINGS).map(heading => (
+                        <th key={heading}>{TRANSACTIONS_TABLE_HEADINGS[heading]}</th>
+                    ))
+                }
+            </tr>
+        </thead>
+        <tbody>
+            {
+                rewardPointsData.map(customer => (
+                    customer.transactions.map(transaction => (
+                        <tr key={`${customer.customerId}-${transaction.transactionId}`}>
+                            <td>{transaction.transactionId}</td>
+                            <td>{customer.customerName}</td>
+                            <td>{transaction.transactionDate}</td>
+                            <td>{transaction.product}</td>
+                            <td>${transaction.amount}</td>
+                            <td>{transaction.rewardPoints}</td>
+                        </tr>
+                    ))
+                ))
+            }
+        </tbody>
+    </table>);
+}
+
+export default CustomerTransactions;
